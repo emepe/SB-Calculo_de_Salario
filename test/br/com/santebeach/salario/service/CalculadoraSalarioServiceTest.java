a13c87500e197fd2655eccf4374f366e859fcafe
JoaoGabrielOT <joao_gabrielot@hotmail.com>
2026-06-16 22:36:13 -0300

    Atualização do projeto

--- test/br/com/santebeach/salario/service/CalculadoraSalarioServiceTest.java
+++ test/br/com/santebeach/salario/service/CalculadoraSalarioServiceTest.java
@@ -13,6 +13,7 @@ import br.com.santebeach.salario.model.Adiantamento;
 import br.com.santebeach.salario.model.ConsumoInterno;
 import br.com.santebeach.salario.model.Funcionario;
 import br.com.santebeach.salario.model.Produto;
+import br.com.santebeach.salario.model.RegraDeDesconto;
 import br.com.santebeach.salario.service.CalculadoraSalarioService;
 
 public class CalculadoraSalarioServiceTest {
@@ -20,11 +21,10 @@ public class CalculadoraSalarioServiceTest {
     @Test
     public void deveCalcularSalarioSemAdiantamentoNemConsumo() {
         Funcionario func = new Funcionario("Func 1", 2500.0);
-        List<Adiantamento> adiantamentos = new ArrayList<>();
-        List<ConsumoInterno> consumos = new ArrayList<>();
+        List<RegraDeDesconto> descontos = new ArrayList<>();
         CalculadoraSalarioService calculadora = new CalculadoraSalarioService();
 
-        double resultado = calculadora.calcularSalarioFinal(func, adiantamentos, consumos, Month.JUNE, 2026);
+        double resultado = calculadora.calcularSalarioFinal(func, descontos, Month.JUNE, 2026);
 
         assertEquals(2500.0, resultado);
     }
@@ -32,14 +32,13 @@ public class CalculadoraSalarioServiceTest {
     @Test
     public void deveDescontarAdiantamentosDoSalario() {
         Funcionario func = new Funcionario("Func 1", 2500.0);
-        List<Adiantamento> adiantamentos = new ArrayList<>();
-        List<ConsumoInterno> consumos = new ArrayList<>();
+        List<RegraDeDesconto> descontos = new ArrayList<>();
         CalculadoraSalarioService calculadora = new CalculadoraSalarioService();
 
-        adiantamentos.add(new Adiantamento(func, LocalDate.of(2026, 6, 5), 300.0));
-        adiantamentos.add(new Adiantamento(func, LocalDate.of(2026, 6, 10), 200.0));
+        descontos.add(new Adiantamento(func, LocalDate.of(2026, 6, 5), 300.0));
+        descontos.add(new Adiantamento(func, LocalDate.of(2026, 6, 10), 200.0));
 
-        double resultado = calculadora.calcularSalarioFinal(func, adiantamentos, consumos, Month.JUNE, 2026);
+        double resultado = calculadora.calcularSalarioFinal(func, descontos, Month.JUNE, 2026);
 
         assertEquals(2000.0, resultado);
     }
@@ -47,16 +46,15 @@ public class CalculadoraSalarioServiceTest {
     @Test
     public void deveDescontarConsumoInternoDoSalario() {
         Funcionario func = new Funcionario("Func 1", 2500.0);
-        List<Adiantamento> adiantamentos = new ArrayList<>();
-        List<ConsumoInterno> consumos = new ArrayList<>();
+        List<RegraDeDesconto> descontos = new ArrayList<>();
         CalculadoraSalarioService calculadora = new CalculadoraSalarioService();
 
-        Produto espeto = new Produto("Espeto", 10.0); // custo de exemplo
+        Produto espeto = new Produto("Espeto", 10.0);
 
         // 2 unidades → custo 20.0 → desconto 20 * 1.03 = 20.6
-        consumos.add(new ConsumoInterno(func, espeto, LocalDate.of(2026, 6, 3), 2));
+        descontos.add(new ConsumoInterno(func, espeto, LocalDate.of(2026, 6, 3), 2));
 
-        double resultado = calculadora.calcularSalarioFinal(func, adiantamentos, consumos, Month.JUNE, 2026);
+        double resultado = calculadora.calcularSalarioFinal(func, descontos, Month.JUNE, 2026);
 
         assertEquals(2500.0 - 20.0 * 1.03, resultado);
     }
@@ -64,18 +62,17 @@ public class CalculadoraSalarioServiceTest {
     @Test
     public void deveDescontarAdiantamentosEConsumoJuntos() {
         Funcionario func = new Funcionario("Func 1", 2500.0);
-        List<Adiantamento> adiantamentos = new ArrayList<>();
-        List<ConsumoInterno> consumos = new ArrayList<>();
+        List<RegraDeDesconto> descontos = new ArrayList<>();
         CalculadoraSalarioService calculadora = new CalculadoraSalarioService();
 
-        adiantamentos.add(new Adiantamento(func, LocalDate.of(2026, 6, 5), 300.0));
+        descontos.add(new Adiantamento(func, LocalDate.of(2026, 6, 5), 300.0));
 
-        Produto refrigerante = new Produto("Refrigerante", 5.0); // custo de exemplo
+        Produto refrigerante = new Produto("Refrigerante", 5.0);
         // 3 unidades → custo 15.0 → desconto 15 * 1.03 = 15.45
-        consumos.add(new ConsumoInterno(func, refrigerante, LocalDate.of(2026, 6, 3), 3));
+        descontos.add(new ConsumoInterno(func, refrigerante, LocalDate.of(2026, 6, 3), 3));
 
-        double resultado = calculadora.calcularSalarioFinal(func, adiantamentos, consumos, Month.JUNE, 2026);
+        double resultado = calculadora.calcularSalarioFinal(func, descontos, Month.JUNE, 2026);
 
         assertEquals(2500.0 - 300.0 - 15.0 * 1.03, resultado);
-    }    
+    }
 }
